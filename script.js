ed91f90bbc7a64ce23274e75f758a2d859682fd4
poputiSSAM <choiys219@gmail.com>
2024-05-24 19:19:24 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function openVideo(url) {
+    var modal = document.getElementById("videoModal");
+    var iframe = document.getElementById("videoFrame");
+    iframe.src = url;
+    modal.style.display = "block";
+}
+
+function closeVideo() {
+    var modal = document.getElementById("videoModal");
+    var iframe = document.getElementById("videoFrame");
+    iframe.src = "";
+    modal.style.display = "none";
+}
+
+// Close the modal when clicking outside of it
+window.onclick = function(event) {
+    var modal = document.getElementById("videoModal");
+    if (event.target == modal) {
+        closeVideo();
+    }
+}
